f33de033fa94fed9a62ce513d128e4b0ad539a15
Michele Zanella <michele.zanella@polimi.it>
2026-08-17 15:12:52 +0200

    [class1] Adding p5js example

---
+++ class1/js/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400); // create a 400x600 window
+}
+
+function draw() {
+  background(100); // dark blue background (like space)
+
+  circle(200, 200, 100); // draw a circle at (200, 200) with a diameter of 100
+}
